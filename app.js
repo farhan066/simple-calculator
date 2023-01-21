fac6f3933a42ae2b6ec8d6eab9da3dc6483ecd46
Farhan <farhan06.contact@gmail.com>
2023-01-21 08:15:06 +0600

    js updated

--- app.js
+++ app.js
@@ -1,167 +1,101 @@
 class Calculator{
-    constructor(previusNumberBox, currentNumberBox){
-        this.prevNumBox = previusNumberBox;
-        this.currentNumBox = currentNumberBox;
-        this.clear();
+    constructor(prevNumBox, currentNumBox){
+        this.prevNumBox = prevNumBox;
+        this.currentNumBox = currentNumBox;
+        this.clear()
     }
     clear(){
-        this.prevNum = "";
-        this.currentNum = "";
-        this.operation = undefined;
+        this.prevNum = ""
+        this.currentNum = ""
+        this.operator = undefined;
     }
     delete(){
-        this.currentNum = this.currentNum.toString().slice(0,-1)
+        this.currentNum = this.currentNum.slice(0,-1)
     }
     addNumber(number){
-        if(number == "." && this.currentNum.includes(".")){
-            return
-        }
-        this.currentNum = this.currentNum.toString() + number.toString();
+        if(number == "." && this.currentNum.includes(".")) return
+        this.currentNum = this.currentNum.toString() + number.toString()
     }
-    addOperation(operation){
+    addOperator(operator){
         if(this.currentNum == "") return
         if(this.prevNum != ""){
-            this.calc()
+                this.calc()
         }
-        this.operation = operation;
+        this.operator = operator;
         this.prevNum = this.currentNum;
-        this.currentNum = ""
+        this.currentNum = "";
     }
     calc(){
-       let result;
-       const prev = parseFloat(this.prevNum)
-       const current = parseFloat(this.currentNum);
-        if(isNaN(prev) || isNaN(current)) return
-       switch(this.operation){
-        case "+":
-            result = prev + current;
-            break;
-        case "-":
-            result = prev - current;
-            break;
-        case "*":
-            result = prev * current;
-            break;
-        case "/":
-            result = prev / current;
-            break;
-
-        default:
-            return;
-       }
-       this.currentNum = result;
-       this.operation = undefined;
-       this.prevNum = "";
-    }
-    organizeNum(number){
-        return number
+        let result;
+        const prev = parseFloat(this.prevNum);
+        const current = parseFloat(this.currentNum);
+        switch(this.operator){
+            case "+":
+                result = prev + current
+                break;
+            case "-":
+                result = prev - current
+                break;
+            case "/":
+                result = prev / current
+                break;
+            case "*":
+                result = prev * current
+                break;
+            default:
+                 return;
+        }
+        this.currentNum = result;
+        this.operator = undefined;
+        this.prevNum = "";
     }
     updateDisplay(){
-        this.currentNumBox.innerText = this.organizeNum(this.currentNum)
-        if(this.operation != null){
-            this.prevNumBox.innerText = `${this.prevNum} ${this.operation}`
+        if(this.operator != null){
+            this.prevNumBox.innerText = this.prevNum + " " + this.operator
         }else{
-            this.prevNumBox.innerText = ""
+            this.prevNumBox.innerText = "";
         }
+        this.currentNumBox.innerText = this.currentNum
     }
 }
 
-////selection////
 const numberBtns = document.querySelectorAll(".btn-number");
-const operationBtns = document.querySelectorAll(".btn-operator");
-const clearBtn = document.querySelector(".btn-clear");
+const operatorBtns = document.querySelectorAll(".btn-operator");
 const deleteBtn = document.querySelector(".btn-del");
+const clearBtn = document.querySelector(".btn-clear");
 const equalsBtn = document.querySelector(".btn-equal");
 const prevNumBox = document.querySelector("[data-prev]");
 const currentNumBox = document.querySelector("[data-current]");
 
 const calculator = new Calculator(prevNumBox, currentNumBox);
 
-numberBtns.forEach((btn)=>{
-    btn.addEventListener("click", ()=>{
-        calculator.addNumber(btn.dataset.num);
+numberBtns.forEach(button =>{
+    button.addEventListener("click", () => {
+        calculator.addNumber(button.dataset.num)
         calculator.updateDisplay()
     })
 })
-operationBtns.forEach((btn)=>{
-    btn.addEventListener("click", ()=>{
-        calculator.addOperation(btn.dataset.num);
+
+
+clearBtn.addEventListener("click", ()=>{
+    calculator.clear()
+    calculator.updateDisplay()
+})
+deleteBtn.addEventListener("click", ()=>{
+    calculator.delete()
+    calculator.updateDisplay()
+})
+operatorBtns.forEach(operator =>{
+    operator.addEventListener("click", ()=>{
+        calculator.addOperator(operator.dataset.num)
         calculator.updateDisplay()
     })
 })
-equalsBtn.addEventListener("click",()=>{
-   calculator.calc()
-   calculator.updateDisplay()
-})
-clearBtn.addEventListener("click",()=>{
-   calculator.clear()
-   calculator.updateDisplay()
-})
-deleteBtn.addEventListener("click",()=>{
-   calculator.delete()
-   calculator.updateDisplay()
+
+equalsBtn.addEventListener("click", ()=>{
+    calculator.calc()
+    calculator.updateDisplay()
 })
 
 
-
-
-
-
-
-
-console.warn("This project is done by Farhan")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////=========EASY WAY=========///////////
-// const buttons = document.querySelectorAll(".btn");
-// const screen = document.querySelector(".screen");
-// const equal = document.querySelector(".btn-equal");
-// const clear = document.querySelector(".btn-clear");
-// const del = document.querySelector(".btn-del");
-
-
-// buttons.forEach(function(button){
-//     button.addEventListener("click", function(e){
-//         let value = e.currentTarget.dataset.num;
-
-//         screen.value += value
-//     })
-// })
-
-// equal.addEventListener("click", function(){
-//     if(screen.value == ""){
-//         screen.value = ""
-//     }else{
-//         try {
-//             let answer = eval(screen.value)
-//             screen.value = answer;
-//           } catch (error) {
-//             screen.value = "syntax error";
-//           }
-//     }
-// })
-
-
-// clear.addEventListener("click", function(){
-//     screen.value = "";
-// })
-
-// del.addEventListener("click", function(){
-//     let value = screen.value;
-//     screen.value = value.slice(0,-1)
-// })
+console.warn("This project is done by Farhan");
